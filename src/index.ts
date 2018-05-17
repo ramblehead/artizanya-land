68e112d40f458f028393fc95b0c5216cd7b4a7b0
ramblehead <v.rybynok@gmail.com>
2018-05-18 00:28:51 +0100

    Checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
 // -*- coding: utf-8 -*-
 
 import schema from './schema';
+
 import * as graphql from 'graphql';
+import { makeExecutableSchema } from 'graphql-tools';
+
 import * as createGraphqlRouter from '@arangodb/foxx/graphql';
 
 import schemaExequtable from './graphql/schema';
-const sss = schemaExequtable;
+
+// // Some fake data
+// const books = [
+//   {
+//     title: 'Harry Potter and the Sorcerer\'s stone',
+//     author: 'J.K. Rowling',
+//   },
+//   {
+//     title: 'Jurassic Park',
+//     author: 'Michael Crichton',
+//   },
+// ];
+
+// // The GraphQL schema in string form
+// const schemaString = `
+//   type Query { books: [Book] }
+//   type Book { title: String, author: String }
+// `;
+
+// // The resolvers
+// const resolvers = {
+//   Query: { books: () => books },
+// };
+
+// // Put together a schema
+// let schemaExequtable = makeExecutableSchema({
+//   typeDefs: schemaString,
+//   resolvers,
+// });
+
 
 // This is a regular Foxx router.
 const router = createGraphqlRouter({
-  schema,
+  // schema,
+  schema: schemaExequtable,
   graphiql: true,
   graphql: graphql,
 }).summary('GraphQL endpoint')
