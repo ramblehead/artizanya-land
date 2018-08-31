e69b6bda721c765fe6c3d6926df6200e6bef786a
ramblehead <v.rybynok@gmail.com>
2018-09-01 00:23:51 +0100

    Checkpoint

--- .project/setup.el
+++ .project/setup.el
@@ -3,13 +3,8 @@
 (set (make-local-variable 'tide-tsserver-executable)
      (concat (rh-project-get-root) "node_modules/.bin/tsserver"))
 
-;; (set (make-local-variable 'tern-command)
-;;      (list (concat (rh-project-get-root) "node_modules/.bin/tern")))
-
 (set (make-local-variable 'flycheck-typescript-tslint-executable)
      (concat (rh-project-get-root) "node_modules/.bin/tslint"))
-
-;; (set (make-local-variable 'rh-tern-argument-hints-enabled) nil)
 
 ;; TODO: Test NODE_PATH and only add missing paths instead
 ;;       of overwriting all.
@@ -23,7 +18,7 @@
   (when project-root
     (setq file-rpath (file-relative-name buffer-file-name project-root))
     (cond ((string-match-p "\\.ts\\'\\|\\.tsx\\'" file-rpath)
-           (rh-typescript-setup))
+           (rh-setup-typescript-tide))
           ((or (string-match-p "^#!.*node" (save-excursion
                                              (goto-char (point-min))
                                              (thing-at-point 'line t)))
@@ -31,4 +26,4 @@
            (setq rh-js2-additional-externs
                  (append rh-js2-additional-externs
                          '("require" "module" "exports")))
-           (rh-javascript-setup)))))
+           (rh-setup-javascript-tide)))))
