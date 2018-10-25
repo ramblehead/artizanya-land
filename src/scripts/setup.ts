6637a848613c55116ca7b5c2ca2e5e0eba668c96
ramblehead <ramblehead@ramblespace>
2018-10-25 23:28:06 +0100

    Checkpoint

--- src/scripts/setup.ts
+++ src/scripts/setup.ts
@@ -2,89 +2,21 @@
 
 import { db } from '@arangodb';
 
+// class ComponentGenesis {
+//   static readonly NATIVE  = 0;
+//   static readonly FOREIGN = 1;
+//   static readonly NATURAL = 2;
+// }
+
 interface Element {
   _key: string;
   name: string;
   description: string;
   origin: string;
-  processes: any[];
   alternatives: Element[];
 }
 
-const elementsArray: Element[] = [
-  {
-    _key: '0000',
-    name: '17HS4401',
-    description: '',
-    origin: 'MotionKing (China) Motor Industry',
-    processes: [],
-    alternatives: [],
-  },
-  {
-    _key: '0001',
-    name: '3D076',
-    description: 'GT2 20T Belt Pulley',
-    origin: 'WODE',
-    processes: [],
-    alternatives: [],
-  },
-  {
-    _key: '0002',
-    name: 'M3 30mm Cap Screw',
-    description: 'M3 30mm Cap Screw',
-    origin: '',
-    processes: [],
-    alternatives: [],
-  },
-  {
-    _key: '0003',
-    name: 'M3 12mm Cap Screw',
-    description: 'M3 12mm Cap Screw',
-    origin: '',
-    processes: [],
-    alternatives: [],
-  },
-  {
-    _key: '0004',
-    name: 'M3 Self Locking Nut',
-    description: 'M3 Self Locking Nut',
-    origin: '',
-    processes: [],
-    alternatives: [],
-  },
-  {
-    _key: '0005',
-    name: 'LM8UU',
-    description: '8mm Linear Ball Bearing',
-    origin: '',
-    processes: [],
-    alternatives: [],
-  },
-  {
-    _key: '0006',
-    name: 'X Motor Printed Part, Leadscrews',
-    description: '',
-    processes: [],
-    origin: 'HTA3D',
-    alternatives: [],
-  },
-  {
-    _key: '0007',
-    name: 'Xmotor Assembly, Leadscrews',
-    description: '',
-    origin: 'HTA3D',
-    processes: ['processes/0001'],
-    alternatives: [],
-  }
-];
-
-interface Input {
-  _key: string;
-  count: number;
-  element: Element;
-}
-
-interface Output {
+interface Component {
   _key: string;
   count: number;
   element: Element;
@@ -96,11 +28,97 @@ interface Process {
   description: string;
   // tools: any[];
   // skills: any[];
-  outputs: Input[];
-  inputs: Output[];
+  outputs: Component[];
+  inputs: Component[];
   // origin: string;
   alternatives: Process[];
 }
+
+
+const elementsArray: Element[] = [
+  {
+    _key: '0000',
+    name: '17HS4401',
+    description: '',
+    origin: 'MotionKing (China) Motor Industry',
+    alternatives: [],
+  },
+  {
+    _key: '0001',
+    name: '3D076',
+    description: 'GT2 20T Belt Pulley',
+    origin: 'WODE',
+    alternatives: [],
+  },
+  {
+    _key: '0002',
+    name: 'M3 30mm Cap Screw',
+    description: 'M3 30mm Cap Screw',
+    origin: '',
+    alternatives: [],
+  },
+  {
+    _key: '0003',
+    name: 'M3 12mm Cap Screw',
+    description: 'M3 12mm Cap Screw',
+    origin: '',
+    alternatives: [],
+  },
+  {
+    _key: '0004',
+    name: 'M3 Self Locking Nut',
+    description: 'M3 Self Locking Nut',
+    origin: '',
+    alternatives: [],
+  },
+  {
+    _key: '0005',
+    name: 'LM8UU',
+    description: '8mm Linear Ball Bearing',
+    origin: '',
+    alternatives: [],
+  },
+  {
+    _key: '0006',
+    name: 'X Motor Printed Part, Leadscrews',
+    description: '',
+    origin: 'HTA3D',
+    alternatives: [],
+  },
+  {
+    _key: '0007',
+    name: 'Xmotor Assembly, Leadscrews',
+    description: '',
+    origin: 'HTA3D',
+    alternatives: [],
+  },
+  {
+    _key: '0009',
+    name: 'GBP',
+    description: 'UK Money',
+    origin: 'The UK',
+    alternatives: [],
+  }
+];
+
+const processesArray: Process[] = [
+  {
+    _key: '0000',
+    name: 'Xmotor Assembly, Leadscrews',
+    description: '',
+    outputs: [],
+    inputs: [],
+    alternatives: [],
+  },
+  {
+    _key: '0001',
+    name: 'Purchase',
+    description: '',
+    outputs: [],
+    inputs: [],
+    alternatives: [],
+  }
+];
 
 // let categories = [{
 //   category: 'Electronic',
@@ -194,6 +212,7 @@ interface Process {
 // }];
 
 const elementsCollectionName = module.context.collectionName('elements');
+
 if(!db._collection(elementsCollectionName)) {
   const elements = db._createDocumentCollection(elementsCollectionName);
   elementsArray.forEach((element: Element) => {
@@ -205,26 +224,44 @@ else if(module.context.isProduction) {
 already exists. Leaving it untouched.`);
 }
 
-const elementsOrderCollectionName = module.context.collectionName('elementsOrder');
-if(!db._collection(elementsOrderCollectionName)) {
-  const elementsOrder = db._createEdgeCollection(elementsOrderCollectionName);
+const processesCollectionName = module.context.collectionName('processes');
+
+if(!db._collection(processesCollectionName)) {
+  const processes = db._createDocumentCollection(processesCollectionName);
+  processesArray.forEach((process: Process) => {
+    processes.save(process);
+  });
+}
+else if(module.context.isProduction) {
+  console.warn(`collection ${processesCollectionName} \
+already exists. Leaving it untouched.`);
+}
+
+const processesInputsCollectionName =
+  module.context.collectionName('processesInputs');
+
+if(!db._collection(processesInputsCollectionName)) {
+  const processesInputs =
+    db._createEdgeCollection(processesInputsCollectionName);
+
   [
-    ['0000', '0001'],
-    ['0001', '0002'],
-    ['0002', '0003'],
-    ['0003', '0004'],
-    ['0004', '0005'],
-    ['0005', '0006'],
-    ['0006', '0007'],
+    ['0000', '0000'],
+    ['0001', '0000'],
+    ['0002', '0000'],
+    ['0003', '0000'],
+    ['0004', '0000'],
+    ['0005', '0000'],
+    ['0006', '0000'],
   ].forEach(pair => {
-    elementsOrder.save(
+    processesInputs.save(
       elementsCollectionName + '/' + pair[0],
       elementsCollectionName + '/' + pair[1],
       {}
     );
   });
+
 }
 else if(module.context.isProduction) {
-  console.warn(`collection ${elementsOrderCollectionName} \
+  console.warn(`collection ${processesInputsCollectionName} \
 already exists. Leaving it untouched.`);
 }
